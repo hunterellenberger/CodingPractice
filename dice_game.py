fa551add8db39e96349e69e345a53b78a7142590
Hunter Ellenberger <hunter@Hunters-MacBook-Pro.local>
2025-10-25 01:14:24 -0400

    finished kattis dice game

---
+++ dice_game.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    gunnar = list(map(int, input().split()))
+    emma = list(map(int, input().split()))
+    if sum(gunnar) > sum(emma):
+        print("Gunnar")
+    elif sum(gunnar) < sum(emma):
+        print("Emma")
+    else:
+        print("Tie")
