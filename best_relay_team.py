208d477818ff1601c0764b5f8a1073d2d470fb50
Hunter <helly10@outlook.com>
2025-10-27 23:31:46 -0400

    continuing best relay team

--- best_relay_team.py
+++ best_relay_team.py
@@ -1,8 +1,32 @@
 if __name__ == "__main__":
     people = int(input())
-    racers = []
+    racers = list()
+    finalTeam = []
+    first = []
+    other = []
+
     for _ in range(people):
         racer = input().split()
         racers.append(racer)
+        first.append(float(racer[1]))
+        other.append(float(racer[2]))
 
-    print(racers)
+    firstTotal = sum(first)
+    otherTotal = sum(other)
+    first.sort()
+    other.sort()
+
+    for _ in range(4):
+        if first[0] / firstTotal > other[0] / otherTotal:
+            for racer in racers:
+                print(racer)
+                if first[0] == float(racer[1]):
+                    finalTeam.append(racer)
+                    break
+            first.pop(0)
+        else:
+            for racer in racers:
+                if other[0] == float(racer[2]):
+                    finalTeam.append(racer[0])
+                    break
+            other.pop(0)
